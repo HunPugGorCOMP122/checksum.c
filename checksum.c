42eee80b1916b9f67dc5106af51b0004d36528cb
hunter emerson gorcey <78040049+HunPugGorCOMP122@users.noreply.github.com>
2021-02-14 19:40:05 +0000

    Create checksum.c

---
+++ checksum.c
@@ -0,0 +1,66 @@
+/********************************/
+/* Program Name:checksum.c      */
+/* Author:Hunter Gorcey         */
+/* Date:2/14/2021               */
+/********************************/
+/* Description:                 */
+/* Validation Checks:           */
+/* Enhancements:                */
+/********************************/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include <unistd.h>
+
+#define max_int (255)
+#define byte (char)
+
+int main(int argc, char * argv[], char ** envp)
+{
+
+	int count = 10;
+	int sum = 0;
+	int checksum;
+	int complement;
+	int quotient;
+	int remainder;
+	int input;
+
+	for (int c = 1; c <= count; c++)
+	{
+		scanf_s("%d", &input);
+		if (input > max_int)
+		{
+			fprintf(stderr, "Error in input \n");
+			exit(1);
+		}
+
+		if (c == 10)
+		{
+			checksum = input;
+			input = 0;
+		}
+
+		sum += input;
+	}
+
+	quotient = sum / (max_int + 1);
+	remainder = sum & (max_int + 1);
+	sum = quotient + remainder;
+	complement = max_int - sum;
+
+	/* the following is the prototype for the read system call */
+	int read(int fildes, void *buf, size_t nbyte);  
+	int buf[10];
+	int ret;
+	ret = read(0, (void*) &buf, 10);
+
+	printf("%s\n", buf);
+	
+	fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
+	if (checksum != complement) {
+		fprintf(stderr, "Error Detected!\n");
+		return 1;
+	}
+	return 0;
+}
